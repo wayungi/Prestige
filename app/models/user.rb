6ae2170eab618a27f1c4509859c89bb6f595f324
wayungi <fwayungi@gmail.com>
2023-04-01 00:03:05 +0300

    ADD: associations between users, themes & vehicles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :themes
+    has_many :vehicles, through: :themes
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,4 @@
 class Vehicle < ApplicationRecord
+    has_many :themes
+    has_many :users, through: :themes
 end
