29f14cfb6a20a059884ac6f2994b59fab153e1bb
wayungi <fwayungi@gmail.com>
2023-03-31 14:07:02 +0300

    ADD:resource route for vehicles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Vehicle.create(name:'Vitz', image:'https://picsum.photos/200', drive_fee:15000, purchase_fee:15000000)
+
+Vehicle.create(name:'Alphard', image:'https://picsum.photos/200', drive_fee:18000, purchase_fee:46000000)
+Vehicle.create(name:'Rav4', image:'https://picsum.photos/200', drive_fee:20000, purchase_fee:30000000)
+Vehicle.create(name:'Corsa', image:'https://picsum.photos/200', drive_fee:12000, purchase_fee:10000000)
+Vehicle.create(name:'Subaru', image:'https://picsum.photos/200', drive_fee:25000, purchase_fee:36000000)
+Vehicle.create(name:'Lancer', image:'https://picsum.photos/200', drive_fee:30000, purchase_fee:45000000)
+
