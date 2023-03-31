71e64eda4d0fde8778fc3ff3a1b39cfeeb10cb08
wayungi <fwayungi@gmail.com>
2023-03-31 14:30:25 +0300

    ADD: scaffold user

---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "users/show", type: :view do
+  before(:each) do
+    assign(:user, User.create!(
+      name: "Name",
+      password: "Password",
+      email: "Email",
+      contact: "Contact"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/Password/)
+    expect(rendered).to match(/Email/)
+    expect(rendered).to match(/Contact/)
+  end
+end
