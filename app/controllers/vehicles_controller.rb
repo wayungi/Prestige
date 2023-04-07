dc9bf5b8b45814232d1497b49411a2caaa34873b
wayungi <fwayungi@gmail.com>
2023-04-07 19:56:51 +0300

    UPDATE:index view

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,13 +1,6 @@
 class VehiclesController < ApplicationController
   before_action :set_vehicle, only: %i[show edit update destroy]
 
-  # GET /vehicles
-  # browser route: http://127.0.0.1:3000/users/1/vehicles/theme
-  # def theme
-  #   @user = User.find(1) #! should change this user on add devise
-  #   @vehicles = @user.vehicles
-  # end
-
   # GET /vehicles or /vehicles.json
   def index
     @user = User.find(1) #! should change this user on add devise
