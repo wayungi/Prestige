f245b2203c1bda2ffc1102ec83173c72725f9bc4
wayungi <fwayungi@gmail.com>
2023-04-13 23:40:16 +0300

    ADD: temporarily disable using ative storage images

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,6 @@
 class Vehicle < ApplicationRecord
   has_many :themes
   has_many :users, through: :themes
-  has_one_attached :image
+  # ! this will need to be changed when  the images are comming from the db
+  # has_one_attached :image
 end
