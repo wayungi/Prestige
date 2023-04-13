00786b35e1343ed5d6b0f7a2c72f5a0caccc41f7
wayungi <fwayungi@gmail.com>
2023-04-13 23:55:39 +0300

    ADD: reserve_form route

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
   resources :users
   resources :vehicles do
     post 'reserve', on: :member #creates a custom => route vehicles/11/reserve
+    get 'reserve_form', on: :member #creates a custom => route vehicles/11/reserve_form
   end
   get 'vehicles/delete/candidates', to: 'vehicles#candidates'
+
 
 
 
