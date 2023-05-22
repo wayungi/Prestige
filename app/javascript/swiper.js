81b48dda7069393177ec8c9907d935778c0a55e1
wayungi <fwayungi@gmail.com>
2023-05-22 22:43:01 +0300

    FIX: application height

--- app/javascript/swiper.js
+++ app/javascript/swiper.js
@@ -1,1 +1,29 @@
-console.log("swiper included");
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // speed: 400,
+    // spaceBetween: 100,
+
+    direction: 'horizontal',
+    loop: true,
+    enabled: true,
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    spaceBetween: 10,
+  
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+  
