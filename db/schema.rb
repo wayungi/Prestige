353003c02ce1fd9d57b9ec235250dcccd195b447
wayungi <fwayungi@gmail.com>
2023-05-29 23:04:57 +0300

    FIX: fix migrtion to add cascade on delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_12_205613) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_200053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,5 +75,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_12_205613) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "themes", "users"
-  add_foreign_key "themes", "vehicles"
+  add_foreign_key "themes", "vehicles", on_delete: :cascade
 end
