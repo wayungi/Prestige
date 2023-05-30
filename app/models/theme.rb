a3527650e7a6d162a7c65408abc91347871354fe
wayungi <fwayungi@gmail.com>
2023-05-30 22:49:46 +0300

    ADD: validation on theme model

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,4 +1,9 @@
 class Theme < ApplicationRecord
   belongs_to :user
   belongs_to :vehicle
+
+  validates :user, presence: true 
+  validates :vehicle, presence: true
+  validates :name, presence: true
+  validates :book_date, presence: true
 end
