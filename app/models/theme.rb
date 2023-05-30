8b9a76c53ede06360da3da398265982e01fdad79
wayungi <fwayungi@gmail.com>
2023-05-30 23:37:43 +0300

    REFACTOR: theme validation

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,9 +1,5 @@
 class Theme < ApplicationRecord
   belongs_to :user
   belongs_to :vehicle
-
-  validates :user, presence: true 
-  validates :vehicle, presence: true
-  validates :name, presence: true
-  validates :book_date, presence: true
+  validates :user, :vehicle, :name, :book_date, presence: true 
 end
