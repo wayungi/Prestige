5bf83914195c28b44e0b3a8e7615865fee1b3cda
wayungi <fwayungi@gmail.com>
2023-05-31 00:13:18 +0300

    ADD: uniqueness of user in vehicle scope for theme model

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -2,4 +2,5 @@ class Theme < ApplicationRecord
   belongs_to :user
   belongs_to :vehicle
   validates :user, :vehicle, :name, :book_date, presence: true 
+  validates :user, uniqueness: {scope: :vehicle}
 end
