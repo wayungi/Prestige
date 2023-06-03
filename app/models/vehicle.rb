88eb2877b7fd9ca717a6fdb097cb900e43c2c46f
wayungi <fwayungi@gmail.com>
2023-06-03 13:13:52 +0300

    UPDATE: uncomment the has_one_attached macro

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,6 +1,5 @@
 class Vehicle < ApplicationRecord
   has_many :themes
   has_many :users, through: :themes
-  # ! this will need to be changed when  the images are comming from the db
-  # has_one_attached :image
+  has_one_attached :image
 end
