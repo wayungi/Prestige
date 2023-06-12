22ae343bdc7dad2226dc8d3957b8bcda047a4364
wayungi <fwayungi@gmail.com>
2023-06-12 23:18:48 +0300

    ADD: new column to themes table

---
+++ db/migrate/20230612201340_add_city_to_themes.rb
@@ -0,0 +1,5 @@
+class AddCityToThemes < ActiveRecord::Migration[7.0]
+  def change
+    add_column :themes, :city, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_200053) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_201340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_200053) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
     t.index ["user_id"], name: "index_themes_on_user_id"
     t.index ["vehicle_id"], name: "index_themes_on_vehicle_id"
   end
